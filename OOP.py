8526af084a6dd95f2eafb4c9ad323eccfdc9b903
Igor <igor.vguerra@hotmail.com>
2025-06-27 23:42:15 +0100

    pillars of OOP

--- OOP.py
+++ OOP.py
@@ -18,3 +18,69 @@ person2 = Person("Carlos", 30)
 message = person2.salute()
 print(message)
 
+
+#inheritance
+
+print("\nExample of inheritance:")
+class Animal:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def walk(self):
+        print(f"The animal {self.name} is walking")
+        return
+    
+    def make_sound(self):
+        pass 
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Woof, woof"
+    
+class Cat(Animal):
+    def make_sound(self):
+        return "Miau"
+    
+dog_1 = Dog(name="Hazel")
+cat_1 = Cat(name="Blacky")
+
+print("\nExample of polymorphism:")
+animals = [dog_1, cat_1]
+for animal in animals:
+    print(f"{animal.name} makes this sound: {animal.make_sound()}")
+
+#encapsulation
+print("\nExample of encapsulation:")
+
+class BankAccount:
+    def __init__(self, balance) -> None:
+        self.__balance = balance #protected with the double underline
+       
+    def deposit(self, value):
+        if value > 0:
+            self.__balance += value
+    
+    def withdraw(self, value):
+        if value > 0 and value <= self.__balance:
+            self.__balance -= value
+
+    def check_balance(self):
+        return self.__balance
+        pass
+
+account = BankAccount(balance = 1000)
+print(f"Your account balance is: ${account.check_balance()}")
+account.deposit(value = 500)
+print(f"Your account balance is: ${account.check_balance()}")
+account.deposit(value = -500)
+print(f"Your account balance is: ${account.check_balance()}")
+account.withdraw(value = 200)
+print(f"Your account balance is: ${account.check_balance()}")
+account.withdraw(value = 2000)
+print(f"Your account balance is: ${account.check_balance()}")
+
+print("\nSecond account")
+account_2 = BankAccount(balance = 50)
+print(f"Your account balance is: ${account_2.check_balance()}")
+account_2.deposit(value = 100)
+print(f"Your account balance is: ${account_2.check_balance()}")
