fd17aede1af69a8ad6ced70f1405dfd94b4bc958
Igor <igor.vguerra@hotmail.com>
2025-06-30 14:18:28 +0100

    project base is lay out

--- Project/game.py
+++ Project/game.py
@@ -12,6 +12,21 @@ class Character:
     
     def get_level(self):
         return self.__level
+    
+    def show_details(self):
+        return f"Name: {self.get_name()}\nLife: {self.get_life()}\nLevel: {self.get_level()}"
+    
+    def receive_attack(self, damage):
+        self.__life -= damage
+        if self.__life < 0:
+            self.__life = 0
+
+    def attack(self, target):
+        damage = self.__level * 2
+        target.receive_attack(damage)
+        print(f"{self.get_name()} attacked {target.get_name()} and gave {damage} of damage!")
+    
+
 
 class Hero(Character):
     def __init__(self, name, life, level, special_skill):
@@ -20,6 +35,8 @@ class Hero(Character):
 
     def get_special_skill(self):
         return self.__special_skill
+    def show_details(self):
+        return f"{super().show_details()}\nSpecial Skill: {self.get_special_skill()}\n"
     
 class Enemy(Character):
     def __init__(self, name, life, level, type):
@@ -29,3 +46,35 @@ class Enemy(Character):
     def get_type(self):
         return self.__type
     
+    def show_details(self):
+        return f"{super().show_details()}\nType: {self.get_type()}"
+    
+class Game:
+    def __init__(self):
+        self.hero = Hero(name="Birdman", life=100, level=5, special_skill="Fly")
+        self.enemy = Enemy(name="Ekans", life=50, level=3, type="Poisonous")
+    
+    def starting_battle(self):
+        print("Battle has started!")
+        while self.hero.get_life() > 0 and self.enemy.get_life() > 0:
+            print("\nCharacters Details")
+            print(self.hero.show_details())
+            print(self.enemy.show_details())
+
+            input("Press Enter to attack...")
+            choice = input("Choose (1 - Normal Attack, 2 - Special Skill):")
+            
+            if choice == "1":
+                self.hero.attack(self.enemy)
+            else:
+                print("Invalid choice. Try again")
+
+        if self.hero.get_life() > 0:
+            print("\nCongratulations, you won the battle!")
+        else:
+            print("\nOh no, you lost the battle!")
+
+
+
+game = Game()
+game.starting_battle()
